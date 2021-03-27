faf612760180831dd65a9d8b7667147df169749f
gnii <chochain@yahoo.com>
2021-03-27 08:27:50 -0700

    ~/arduino port from ~/esp/goof .

--- eforth.h
+++ eforth.h
@@ -1,21 +1,123 @@
 #ifndef __EFORTH_SRC_EFORTH_H
 #define __EFORTH_SRC_EFORTH_H
-#include <avr/pgmspace.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <ESP8266WiFi.h>
+//
+// debugging flags
+//
+#define PRINTF(s, ...)  Serial.printf(s, ##__VA_ARGS__)
+#define GETCHAR()       Serial.read()
+#define ASSEM_DUMP      0
+#define DATA_DUMP       0
+#define FORTH_TRACE     0
+//
+// portable types
+//
+typedef uint64_t  U64;
+typedef uint32_t  U32;
+typedef uint16_t  U16;
+typedef uint8_t   U8;
+typedef int64_t   S64;
 
-typedef uint16_t    U16;
-typedef uint8_t     U8;
-typedef int16_t     S16;
+typedef int32_t   S32;
+typedef int16_t   S16;
+typedef int8_t    S8;
 
-#define	LOGICAL(f) ((f) ? 0xffff : 0)
-#define LOWER(x,y) ((U16)(x)<(U16)(y))
-#define	pop()      { top = *S--; }
-#define	push(v)	   { *++S = top; top = (v); }
-#define ds16       ((U16*)0)
-#define ds8        ((U8*)0)
-
-#define FETCH(a)   (pgm_read_word(cs16+(a)))                     // read from flash
-#define AT(a)      ((a<0x900) ? ds16[a>>1] : FETCH(a>>1))
-#define BYTE(a)    (pgm_read_byte(cs8+(a)))
-#define CAT(a)     ((a<0x900) ? (int)ds8[a] : (int)BYTE(a))
-
+typedef U32       XA;				// Address size
+//
+// capacity and sizing
+//
+#define CELLSZ		     4
+#define FORTH_PRIMITIVES 64
+#define FORTH_RACK_SZ    64
+#define FORTH_STACK_SZ   64
+#define FORTH_DATA_SZ    0x2000
+#define ASSEM_RACK_SZ    64
+//
+// logic and stack op macros (processor dependent)
+//
+#define FORTH_BOOT_ADDR  0x0
+#define FORTH_TVAR_ADDR  0x80
+#define FORTH_UVAR_ADDR  0x90
+#define FORTH_DIC_ADDR   0x200
+#define FORTH_TIB_ADDR   0x100
+#define FORTH_TIB_SZ     0x100
+//
+// TRUE cannot use 1 because NOT(ffffffff)==0 while NOT(1)==ffffffff
+// which does not need boolean op (i.e. in C)
+//
+#define	TRUE	         -1
+#define	FALSE	         0
+//
+// Forth VM Opcodes (for Bytecode Assembler)
+//
+enum {
+    opNOP = 0,    // 0
+    opBYE,        // 1
+    opQRX,        // 2
+    opTXSTO,      // 3
+    opDOCON,      // 4
+    opDOLIT,      // 5
+    opENTER,      // 6
+    opEXIT,       // 7
+    opEXECU,      // 8
+    opDONEXT,     // 9
+    opQBRAN,      // 10
+    opBRAN,       // 11
+    opSTORE,      // 12
+    opAT,         // 13
+    opCSTOR,      // 14
+    opCAT,        // 15
+    opRPAT,       // 16   borrowed for trc_on
+    opRPSTO,      // 17   borrowed for trc_off
+    opRFROM,      // 18
+    opRAT,        // 19
+    opTOR,        // 20
+    opSPAT,       // 21
+    opSPSTO,      // 22   
+    opDROP,       // 23
+    opDUP,        // 24
+    opSWAP,       // 25
+    opOVER,       // 26
+    opZLESS,      // 27
+    opAND,        // 28
+    opOR,         // 29
+    opXOR,        // 30
+    opUPLUS,      // 31
+    opNEXT,       // 32
+    opQDUP,       // 33
+    opROT,        // 34
+    opDDROP,      // 35
+    opDDUP,       // 36
+    opPLUS,       // 37
+    opINVER,      // 38
+    opNEGAT,      // 39
+    opDNEGA,      // 40
+    opSUB,        // 41
+    opABS,        // 42
+    opEQUAL,      // 43
+    opULESS,      // 44
+    opLESS,       // 45
+    opUMMOD,      // 46
+    opMSMOD,      // 47
+    opSLMOD,      // 48
+    opMOD,        // 49
+    opSLASH,      // 50
+    opUMSTA,      // 51
+    opSTAR,       // 52
+    opMSTAR,      // 53
+    opSSMOD,      // 54
+    opSTASL,      // 55
+    opPICK,       // 56
+    opPSTOR,      // 57
+    opDSTOR,      // 58
+    opDAT,        // 59
+    opCOUNT,      // 60
+    opDOVAR,      // 61
+    opMAX,        // 62
+    opMIN         // 63
+};
 #endif // __EFORTH_SRC_EFORTH_H
--- eforth.ino
+++ eforth.ino
@@ -1,178 +1,47 @@
-/******************************************************************************/
-/* Arduino eForth.C, 1.0: For Atmega328 on arduino Uno                        */
-/******************************************************************************/
-/* Chen-Hanson Ting                                                           */
-/* Version 2.0, 06nov11cht                                                    */
-/* Compile new commands in RAM                                                */
-/*                                                                            */
-/* Version 1.0, 21sep11cht                                                    */
-/* Adopted from eForth_11.c                                                   */
-/* Compiled by Arduino as a sketch                                            */
-/* Follow closely the original eForth model                                   */
-/* Kernel has 32 primitives                                                   */
-/* code[] array must be filled with rom.mif produced by cefMETA328            */
-/* @, !, C@ and C! access RAM memory                                          */
-/******************************************************************************/
-#include <stdint.h>
 #include "eforth.h"
 
-extern int cs16[];
-U8   *cs8 = (U8*)cs16;
-int  rack[32]  = {0}, *R = rack;
-int  stack[32] = {0}, *S = stack;
+extern int  assemble(U8 *rom);
+extern void vm_init(U8 *rom);
+extern void vm_run();
 
-int  I, P, IP;
-int  n, w, clock, phase, top;
+U32 data[FORTH_DATA_SZ] = {};           		// 64K forth memory block
 
-void dump(void)
-{
-    Serial.print(clock, HEX);
-    Serial.print(F(":"));
-    Serial.print(IP, HEX);
-    Serial.print(F("]\tP="));
-    Serial.print(P, HEX);
-    Serial.print(F("\tI="));
-    Serial.println(I, HEX);
-    
-    Serial.print(F("S="));      // data stack
-    for (int n=0; n<=(S-stack); n++) {
-        Serial.print(stack[n], HEX);
-        Serial.print(F(" "));
-    } 
-    Serial.println(top, HEX);
-    Serial.print(F("R="));      // return stack
-    for (int n=0; n<=(R-rack); n++) {
-        Serial.print(rack[n], HEX);
-        Serial.print(F(" "));
-    } 
-    Serial.println(F("")); 
-}
-void jump(void)     { clock |= 3;                  } 
-void next(void)     { P=AT(P); IP+=2; jump();      }
-void bye()          { exit(0);                     } 
-void qrx(void)
-{
-    while (!Serial) {};
-    push(Serial.read());
-    push(0xffff);
-}
-void txsto(void)   { Serial.write((U8)top); pop();  } 
-void emit(void)    { txsto();                       } 
-void docon(void)   { push(AT(P));  P+=2;            }
-void dolit(void)   { push(AT(IP)); IP+=2; next();   }
-void dolist(void)  { *++R=IP; IP=P; next();         }
-void exitt(void)   { IP=*R--; next();               } 
-void execu(void)   { *++R=IP; P=top; pop(); jump(); } 
-void donext(void)
-{
-    if(*R) { *R -= 1; IP=AT(IP); }
-    else   { IP += 2; R--;         }
-    next();
-} 
-void qbran(void)   { IP = top ? IP+2 : AT(IP); pop(); next(); }
-void bran(void)    { IP = AT(IP); next();                     }
-void store(void)   { ds16[top>>1] = *S--; pop();              }
-void cstore(void)  { ds8[top] = (char)*S--; pop();            }
-void at(void)      { top = AT(top);     }
-void cat(void)     { top = CAT(top);    }
-void icat(void)    { top = BYTE(top);   } 
-void iat(void)     { top = FETCH(top);  }
-void istore(void)  { pop(); pop();      } 
-void icstore(void) { pop(); pop();      }
-void rfrom(void)   { push(*R--);        } 
-void rat(void)     { push(*R);          }
-void tor(void)     { *++R = top; pop(); } 
-void rpsto(void)   { R = rack;          } 
-void spsto(void)   { S = stack;         } 
-void drop(void)    { pop();             } 
-void dup(void)     { *++S = top;        } 
-void swap(void)    { w = top; top = *S; *S = w; } 
-void over(void)    { push(S[-1]);       } 
-void zless(void)   { top = LOGICAL(top & 0x8000); }
-void andd(void)    { top &= *S--;       } 
-void orr(void)     { top |= *S--;       } 
-void xorr(void)    { top ^= *S--;       }
-void uplus(void)   { *S += top; top = LOWER(*S, top); } 
-void nop(void)     { jump();            } 
-void dovar(void)   { push(P);           }
-
-void (*primitives[])(void) = {
-    /* case 0 */ nop,
-    /* case 1 */ bye, 
-    /* case 2 */ qrx,    
-    /* case 3 */ txsto,  
-    /* case 4 */ docon, 	
-    /* case 5 */ dolit,
-    /* case 6 */ dolist,
-    /* case 7 */ exitt,
-    /* case 8 */ execu,
-    /* case 9 */ donext,
-    /* case 10 */ qbran,
-    /* case 11 */ bran,
-    /* case 12 */ store,
-    /* case 13 */ at,
-    /* case 14 */ cstore,
-    /* case 15 */ cat,
-    /* case 16 */ icat,
-    /* case 17 */ iat,
-    /* case 18 */ rfrom,
-    /* case 19 */ rat,
-    /* case 20 */ tor,
-    /* case 21 */ dovar, 
-    /* case 22 */ next,
-    /* case 23 */ drop,
-    /* case 24 */ dup,
-    /* case 25 */ swap,
-    /* case 26 */ over,
-    /* case 27 */ zless,
-    /* case 28 */ andd,
-    /* case 29 */ orr,
-    /* case 30 */ xorr,
-    /* case 31 */ uplus,
-    /* case 32 */ icat
-};
-
-void execute(U8 icode)
-{
-    if (icode < 33) {
-        primitives[icode]();         // call primitive function
+void dump_data(U8* byte, int len) {
+#if DATA_DUMP
+    for (int p=0; p<len; p+=0x20) {
+        PRINTF("\n%04x: ", p);
+        for (int i=0; i<0x20; i++) {
+        	U8 c = byte[p+i];
+            PRINTF("%02x", c);
+            PRINTF("%s", (i%4)==3 ? " " : "");
+        }
+        for (int i=0; i<0x20; i++) {
+            U8 c = byte[p+i];
+            PRINTF("%c", c ? ((c>32 && c<127) ? c : '_') : '.');
+        }
     }
-    else {
-        Serial.print (F("\nIllegal code="));
-        Serial.print(icode, HEX);
-        Serial.print(F(" P="));
-        Serial.println(P, HEX );
-    }
+#endif // DATA_DUMP
 }
-/* End of eforth.c */
 
 void setup()
-{ 
-	clock = 0;
-	P     = FETCH(0x480);
-	IP    = 0;
-	top   = 0;
+{
+    Serial.begin(115200);
+    
+	U8 *rom = (U8*)data;
+	setvbuf(stdout, NULL, _IONBF, 0);		// autoflush (turn STDOUT buffering off)
 
-    Serial.begin(9600);
-	Serial.print(F("\nStart Arduino\n"));
+	int sz  = assemble(rom);
+	dump_data(rom, sz+0x20);
+
+	PRINTF("\nceForth v4.0 ROM[%04x]\n", sz);
+	vm_init(rom);
 }
 
 void loop()
 {
-    static U16 I;
-
-	if (!Serial.available()) {
-        delay(100);
-        return;
-    }
-    int n = Serial.read();
-    switch((phase=clock&3)) {
-    case 0: I = AT(P); P+=2;   break;          // fetch instruction
-    case 1: execute(I & 0xff); break;
-    case 2: execute(I >> 8);   break;
-    case 3: jump();           
-    }
-    dump();
+    while (!Serial.available());
     
-    clock += 1; 
+	vm_run();
 }
+
+

dd22b91dc9d4e81f58bf4af754a72369b467c4dc
gnii <chochain@yahoo.com>
2021-06-19 10:50:22 -0700

    ~/refactor eforth_core from eforth.ino

---
+++ src/eforth_core.cpp
@@ -0,0 +1,116 @@
+#include "eforth_core.h"
+
+static U8 _ram[FORTH_RAM_SZ] = {};         // 4K forth memory block
+static Stream   *io;                       // console interface
+static task_ptr _task_list  = NULL;
+
+extern U32 forth_rom[];
+
+void ef_prompt()
+{
+    LOG("\r\neForthUNO v1.0");
+}
+
+void sys_info(U8 *cdata) {
+    LOG_H("\r\nRAM_SZ= x",   FORTH_RAM_SZ);
+    LOG_V(", Primitives=", FORTH_PRIMITIVES);
+    LOG_V(", Addr=",       (U16)sizeof(XA)*8);
+    LOG_V("-bit, CELLSZ=", CELLSZ);
+    LOG("\r\nMEMMAP:");
+    LOG_H("\r\n  ROM   x", FORTH_BOOT_ADDR);
+    LOG_H("\r\n  STACK x", FORTH_STACK_ADDR); LOG_H("+", FORTH_STACK_SZ);
+    LOG_H("\r\n  TIB   x", FORTH_TIB_ADDR);   LOG_H("+", FORTH_TIB_SZ);
+    LOG_H("\r\n  USER  x", FORTH_TVAR_ADDR);  LOG_H("+", FORTH_DIC_ADDR-FORTH_TVAR_ADDR);
+    LOG_H("\r\n  DIC   x", FORTH_DIC_ADDR);   LOG_H("+", FORTH_RAM_ADDR+FORTH_RAM_SZ-FORTH_DIC_ADDR);
+
+#if ARDUINO
+    U16 h = (U16)&cdata[FORTH_RAM_SZ];
+    U16 s = (U16)&h;
+    LOG_H("\r\nHEAP=x", h);
+    LOG_H("--> x",    s-h);
+    LOG_H(" <--SP=x", s);
+#endif // ARDUINO
+}
+
+void ef_add_task(char (*task)()) {
+    task_ptr tp = (task_ptr)malloc(sizeof(ef_task));
+
+    tp->task = task;
+    tp->next = _task_list;
+}
+
+#if ARDUINO
+void ef_yield()
+{
+    task_ptr tp = _task_list;
+    while (tp) {
+        PT_SCHEDULE(tp->task());           // steal cycles for hardware tasks
+        tp = tp->next;
+    }
+}
+//
+// delay with yield
+//
+void ef_delay(U32 ms)
+{
+    U32 t = millis() + ms;
+    while (millis()<t) {
+        ef_yield();                        // run hardware tasks while waiting
+    }
+}
+// 
+// console input with yield
+//
+U8 ef_getchar()
+{
+    while (!io->available()) {
+        ef_yield();
+    }
+    return (U8)io->read();
+}
+//
+// output one char to console
+//
+void ef_putchar(char c)
+{
+    io->print(c);
+    if (c=='\n') {
+        io->flush();
+        ef_yield();
+    }
+}
+
+void ef_setup(Stream &io_stream)
+{
+    io = &io_stream;
+
+    sys_info(_ram);
+	vm_init((PGM_P)forth_rom, _ram);
+}
+
+void ef_run()
+{
+    vm_step();
+}
+#else  // ARDUINO
+void ef_yield()         {}
+void ef_delay(U32 ms)   {}
+U8   ef_getchar()	    { return getchar(); }
+void ef_putchar(char c) { printf("%c", c); }
+
+int main(int ac, char* av[])
+{
+	setvbuf(stdout, NULL, _IONBF, 0);		// autoflush (turn STDOUT buffering off)
+	U8 *cdata = (U8*)_ram;
+
+	int sz  = ef_assemble(cdata);
+	ef_dump_rom(cdata, sz+0x20);
+
+	sys_info(cdata);
+	vm_init((PGM_P)forth_rom, _ram);
+	while (vm_step());
+
+	return 0;
+}
+#endif // ARDUINO
+
--- src/eforth_core.h
+++ src/eforth_core.h
@@ -1,54 +1,56 @@
-#ifndef __EFORTH_SRC_EFORTH_H
-#define __EFORTH_SRC_EFORTH_H
+#ifndef __EFORTH_SRC_EFORTH_CORE_H
+#define __EFORTH_SRC_EFORTH_CORE_H
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 #include <stdint.h>
-#include <Arduino.h>
 #ifdef ESP8266
-//#include "user_interface.h"
+#include "user_interface.h"
 #endif // ESP8266
 
-#define EFORTH_16BIT    1
+#define ROM_ONLY        0
 //
 // debugging flags
 //
-#define PRINTF(s, ...)  Serial.printf(s, ##__VA_ARGS__)
-#define GETCHAR()       Serial.read()
 #define ASM_TRACE       0
 #define EXE_TRACE       0
 //
 // portable types
 //
-typedef uint64_t  U64;
-typedef uint32_t  U32;
+typedef uint32_t  U32;              // unsigned integers
 typedef uint16_t  U16;
 typedef uint8_t   U8;
-typedef int64_t   S64;
 
-typedef int32_t   S32;
+typedef int32_t   S32;              // signed integers
 typedef int16_t   S16;
 typedef int8_t    S8;
 
-typedef U16       XA;				// Address size
+typedef U16       XA;				// 16-bit address
 //
 // capacity and sizing
 //
 #define CELLSZ		     2
 #define FORTH_PRIMITIVES 64
-#define FORTH_MEM_SZ     0x2000
-#define FORTH_STACK_SZ   0x40*CELLSZ
+#define FORTH_ROM_SZ     0x1000
+#define FORTH_RAM_SZ     0x500
+#define FORTH_STACK_SZ   0x60*CELLSZ
 #define FORTH_TIB_SZ     0x40
 #define FORTH_PAD_SZ     0x20
 //
+// note:
+//    Forth only needs a few bytes for Arduino auto (on heap), but
+//    Serial TX/RX buffers uses 0x40 * 2 = 128 bytes
+//
 // logic and stack op macros (processor dependent)
 //
-#define FORTH_BOOT_ADDR  0x0
-#define FORTH_TVAR_ADDR  0x4
-#define FORTH_UVAR_ADDR  0x10
-#define FORTH_TIB_ADDR   0x20
-#define FORTH_STACK_ADDR (FORTH_TIB_ADDR+FORTH_TIB_SZ)
-#define FORTH_DIC_ADDR   (FORTH_STACK_ADDR+FORTH_STACK_SZ)
+#define FORTH_BOOT_ADDR  0x0000
+#define FORTH_RAM_ADDR   0x1000
+#define FORTH_STACK_ADDR (FORTH_RAM_ADDR+0x0)
+#define FORTH_STACK_TOP  (FORTH_STACK_ADDR+FORTH_STACK_SZ)
+#define FORTH_TIB_ADDR   (FORTH_STACK_TOP)
+#define FORTH_TVAR_ADDR  (FORTH_TIB_ADDR+FORTH_TIB_SZ)
+#define FORTH_UVAR_ADDR  (FORTH_TVAR_ADDR+0x10)
+#define FORTH_DIC_ADDR   (FORTH_UVAR_ADDR+0x10)
 //
 // TRUE cannot use 1 because NOT(ffffffff)==0 while NOT(1)==ffffffff
 // which does not need boolean op (i.e. in C)
@@ -75,13 +77,13 @@ enum {
     opAT,         // 13
     opCSTOR,      // 14
     opCAT,        // 15
-    opRPAT,       // 16   borrowed for trc_on
-    opRPSTO,      // 17   borrowed for trc_off
+    opONEP,       // 16 Dr. Ting's opRPAT
+    opONEM,       // 17 Dr. Ting's opRPSTO
     opRFROM,      // 18
     opRAT,        // 19
     opTOR,        // 20
-    opONEP,       // 21 Dr. Ting's opSPAT
-    opONEM,       // 22 Dr. Ting's opSPSTO
+    opDELAY,      // 21 Dr. Ting's opSPAT
+    opCLOCK,      // 22 Dr. Ting's opSPSTO
     opDROP,       // 23
     opDUP,        // 24
     opSWAP,       // 25
@@ -91,37 +93,91 @@ enum {
     opOR,         // 29
     opXOR,        // 30
     opUPLUS,      // 31
-    opNEXT,       // 32
+    opDEPTH,      // 32 Dr. Ting's opNEXT (not needed)
     opQDUP,       // 33
     opROT,        // 34
-    opDDROP,      // 35
-    opDDUP,       // 36
+    opLSHIFT,     // 35 Dr. Ting's opDDROP
+    opRSHIFT,     // 36 Dr. Ting's opDDUP
     opPLUS,       // 37
-    opINVER,      // 38
+    opINVERT,     // 38
     opNEGAT,      // 39
-    opDNEGA,      // 40
+    opGREAT,      // 40 Dr. Ting's opDNEGA
     opSUB,        // 41
     opABS,        // 42
     opEQUAL,      // 43
     opULESS,      // 44
     opLESS,       // 45
     opUMMOD,      // 46
-    opMSMOD,      // 47
-    opSLMOD,      // 48
+    opPIN,        // 47 Dr. Ting's opMSMOD
+    opMAP,        // 48 Dr. Ting's opSLMOD
     opMOD,        // 49
     opSLASH,      // 50
-    opUMSTA,      // 51
+    opUMSTAR,     // 51
     opSTAR,       // 52
     opMSTAR,      // 53
-    opSSMOD,      // 54
-    opSTASL,      // 55
+    opDIN,        // 54 Dr. Ting's opSSMOD
+    opDOUT,       // 55 Dr. Ting's opSTASL
     opPICK,       // 56
     opPSTOR,      // 57
-    opDSTOR,      // 58
-    opDAT,        // 59
+    opAIN,        // 58 Dr. Ting's opDSTOR
+    opAOUT,       // 59 Dr. Ting's opDAT
     opCOUNT,      // 60
     opDOVAR,      // 61
     opMAX,        // 62
     opMIN         // 63
 };
-#endif // __EFORTH_SRC_EFORTH_H
+//
+// protothread task declaration
+//
+typedef struct ef_task {
+    char (*task)();
+    struct ef_task *next;
+} *task_ptr;
+//
+// eForth function prototypes
+//
+#if ARDUINO
+
+#include <Arduino.h>
+#include <avr/pgmspace.h>
+#include <pt.h>
+#include <time.h>
+#define LOG(s)              io->print(F(s))
+#define LOG_C(c)            ef_putchar(c)
+#define LOG_V(s, n)         { io->print(F(s)); io->print((S16)n); }
+#define LOG_H(s, n)         { io->print(F(s)); io->print(n, HEX); }
+
+#else
+
+#include <stdlib.h>
+typedef const char          *PGM_P;
+#define pgm_read_byte       *(U8*)
+#define PROGMEM
+#define Stream              char
+#define pinMode(a,b)
+#define digitalRead(p)      (0)
+#define digitalWrite(p,v)
+#define analogRead(p)		(0)
+#define analogWrite(p,v)
+#define map(a,b,c,d,e) 		(0)
+#define millis()       		(0)
+#define PT_SCHEDULE(t) 		(t)
+#define LOG(s)              printf("%s", s)
+#define LOG_C(c)            ef_putchar(c)
+#define LOG_V(s, n)         printf("%s%d", s, n)
+#define LOG_H(s, n)         printf("%s%x", s, n)
+
+#endif // ARDUINO
+
+void vm_init(PGM_P rom, U8 *cdata);
+int  vm_step();
+
+void ef_prompt();
+U8   ef_getchar();
+void ef_putchar(char c);
+void ef_delay(U32 ms);
+
+int  ef_assemble(U8 *cdata);
+void ef_dump_rom(U8 *cdata, int len);
+
+#endif // __EFORTH_SRC_EFORTH_CORE_H

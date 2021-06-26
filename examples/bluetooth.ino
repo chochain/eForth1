89b957df4e79e5062c35d25fd4a1fba0493315b2
gnii <chochain@yahoo.com>
2021-06-25 17:48:19 -0700

    ~/examples/bluetooth remove protothread

--- examples/bluetooth.ino
+++ examples/bluetooth.ino
@@ -1,26 +1,7 @@
 #include "eforth1.h"
 #include <AltSoftSerial.h>
 
-#define PT_DELAY_msec(th, ms)  do {     \
-    static unsigned long t;             \
-    t = millis() + (unsigned long)(ms); \
-    PT_WAIT_UNTIL((th), millis()>=t);   \
-} while(0)
-
-static struct pt ctx_hw;                    // protothread contexts
-PT_THREAD(hw_task())                        // hardward protothread
-{
-    PT_BEGIN(&ctx_hw);
-    while (1) {
-        digitalWrite(LED_BUILTIN, HIGH);
-        PT_DELAY_msec(&ctx_hw, 500);
-        digitalWrite(LED_BUILTIN, LOW);
-        PT_DELAY_msec(&ctx_hw, 500);
-    }
-    PT_END(&ctx_hw);
-}
-
-AltSoftSerial bt;             // default: RX on pin 8, TX on pin 9
+AltSoftSerial bt;            // set RX on 8, TX on 9
 
 void setup()
 {
@@ -31,8 +12,6 @@ void setup()
     delay(1000);
 
 	ef_setup(bt);
-    ef_add_task(hw_task);
-    
     pinMode(LED_BUILTIN, OUTPUT);
 }
 

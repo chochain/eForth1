53db831c78c1b9a3aec947f9399a145e0401a48c
gnii <chochain@yahoo.com>
2021-07-15 11:04:02 -0700

    ~/examples add number prefix

--- examples/0_hello.ino
+++ examples/0_hello.ino
@@ -1,18 +1,11 @@
 #include "eforth1.h"
-#include <AltSoftSerial.h>
-
-AltSoftSerial bt;            // set RX on 8, TX on 9
 
 void setup()
 {
     Serial.begin(115200);
     while (!Serial);          // wait for serial port to connect
 
-    bt.begin(9600);
-    delay(1000);
-
-	ef_setup(bt);
-    pinMode(LED_BUILTIN, OUTPUT);
+    ef_setup();
 }
 
 void loop()

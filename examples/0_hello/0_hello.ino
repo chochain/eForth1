ea8e8927a200e084422c806f389492a60819701a
gnii <chochain@yahoo.com>
2023-01-09 09:29:47 +0800

    ~/examples create subdir (Arduino example compliance)

---
+++ examples/0_hello/0_hello.ino
@@ -0,0 +1,16 @@
+#include "eforth1.h"
+
+void setup()
+{
+    Serial.begin(115200);
+    while (!Serial);          // wait for serial port to connect
+
+    ef_setup();
+}
+
+void loop()
+{
+    ef_run();
+}
+
+

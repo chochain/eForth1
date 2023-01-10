422797cf3b35c8e9e722aefaee1cabc636ea0b6e
gnii <chochain@yahoo.com>
2023-01-10 10:02:58 +0800

    ~/src/eforth1.h ARDUINO only

--- src/eforth1.h
+++ src/eforth1.h
@@ -1,5 +1,7 @@
 #ifndef __EFORTH_EFORTH1_H
 #define __EFORTH_EFORTH1_H
+
+#if ARDUINO
 #include <Arduino.h>
 #include <time.h>
 #include <pt.h>
@@ -7,6 +9,7 @@
 void ef_add_task(char (*task)());
 void ef_setup(Stream &io_stream=Serial);
 void ef_run();
+#endif // ARDUINO
 
 #endif // __EFORTH_EFORTH1_H
 

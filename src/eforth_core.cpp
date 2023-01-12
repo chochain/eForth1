61ecba173b1fff44ab63aea5f77348a31a5bb600
gnii <chochain@yahoo.com>
2023-01-12 19:32:46 +0800

    ~/src/eforth_core.cpp drop user task support

--- src/eforth_core.cpp
+++ src/eforth_core.cpp
@@ -6,22 +6,22 @@
 
 static U8     *_ram;                     ///< forth memory block dynamic allocated
 static Stream *io;                       ///< console interface
-static task_ptr _task_list  = NULL;      ///< user task linked-list
+
 ///
 /// display eForth system information
 ///
-void sys_info(U8 *cdata) {
-    LOG_H("\nRAM_SZ= x",    FORTH_RAM_SZ);
-    LOG_V(", Primitives=",  FORTH_PRIMITIVES);
-    LOG_V(", Addr=",        (U16)sizeof(IU)*8);
-    LOG_V("-bit, CELL=",    CELLSZ);
+void sys_info(U8 *cdata, int sz) {
+	LOG_H("\nROM_SZ=x",   sz);
+    LOG_H("\, RAM_SZ=x",  FORTH_RAM_SZ);
+    LOG_V(", Addr=",      (U16)sizeof(IU)*8);
+    LOG_V("-bit, CELL=",  CELLSZ);
     LOG("-byte\nMemory MAP:");
 
 #if ARDUINO
     U16 h = (U16)&cdata[FORTH_RAM_SZ];
-    U16 s = (U16)&h;
+    U16 s = (U16)&s;
     LOG_H(" heap=x", h);
-    LOG_H("--> x",    s-h);
+    LOG_V("--> ", s - h);
     LOG_H(" <--auto=x", s);
 #endif // ARDUINO
     LOG_H("\n  ROM  :x0000+", FORTH_ROM_SZ);
@@ -29,17 +29,6 @@ void sys_info(U8 *cdata) {
     LOG_H("\n  DIC  :x", FORTH_DIC_ADDR);   LOG_H("+", FORTH_DIC_SZ - FORTH_UVAR_SZ);
     LOG_H("\n  STACK:x", FORTH_STACK_ADDR); LOG_H("+", FORTH_STACK_SZ);
     LOG_H("\n  TIB  :x", FORTH_TIB_ADDR);   LOG_H("+", FORTH_TIB_SZ);
-}
-///
-/// add user defined task to task queue
-///
-void ef_add_task(char (*task)()) {
-    task_ptr tp = (task_ptr)malloc(sizeof(ef_task));
-    
-    tp->task = task;
-    tp->next = _task_list;
-
-    _task_list = tp;                       // linked list
 }
 
 #if ARDUINO
@@ -52,11 +41,6 @@ void ef_yield()
     static U8 n = 0;
     if (++n < 10) return;                 // give more cycles to VM
     n = 0;
-    task_ptr tp = _task_list;
-    while (tp) {
-        PT_SCHEDULE(tp->task());           // steal cycles for hardware tasks
-        tp = tp->next;
-    }
 }
 /// 
 /// console input with yield
@@ -113,10 +97,10 @@ int main(int ac, char* av[])
 {
     setvbuf(stdout, NULL, _IONBF, 0);       // autoflush (turn STDOUT buffering off)
 
-    ef_assemble(_rom);
+    int sz = ef_assemble(_rom);
 
 #if !ASM_ONLY
-    sys_info(_rom);
+    sys_info(_rom, sz);
     _ram = (U8*)malloc(FORTH_RAM_SZ);       ///< forth memory block dynamic allocated
 
     vm_init((char*)_rom, _ram, NULL);

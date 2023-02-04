4811c293222eeff57453c8d398ed8c8a657b1e3c
gnii <chochain@yahoo.com>
2023-02-04 20:56:38 +0800

    ~/src/eforth_vm refactor into .h

--- src/eforth_vm.cpp
+++ src/eforth_vm.cpp
@@ -1,156 +1,32 @@
 /**
  * @file
  * @brief eForth Virtual Machine module
- * Note: indirect threading model - computed label jumping
- *       replaced EfVMSub module - vtable subroutine calling
  *
- * ####eForth Memory map
- *
- * @code
- *     0x0000-0x1fff ROM (8K Flash memory)
- *     0x2000-0x24ff RAM (2K dynamic memory)
- *         0x2000-0x201f User Variables
- *         0x2020-0x23ff User Dictionary
- *         0x2400-0x247f Data/Return Stacks
- *         0x2480-0x24ff TIB (Terminal Input Buffer)
- *         0x2500        heap
- * @endcode
- *
- * ####Data and Return Stack
- *
- * @code
- *            S                   R
- *            |                   |
- *    top -> [S0, S1, S2,..., R1, R0] <- rtop
- * @endcode
- * Note: Dr. Ting uses U8 (0~255) for wrap-around control
+ * Revision Note: Threading model evolution
+ *   202212 EfVMSub module - vtable subroutine calling
+ *   202301 indirect threading - computed label jumping
+ *   202302 direct threading - opcode => code(); continue = $NEXT
  */
-#include "eforth_core.h"
+#include "eforth_vm.h"
 
 namespace EfVM {
 
 static Stream *io;
 ///
-///@name Control
+///@name VM Registers
 ///@{
-IU  PC;                           ///< program counter, IU is 16-bit
 IU  IP;                           ///< instruction pointer, IU is 16-bit, opcode is 8-bit
+IU  PC;                           ///< program counter, IU is 16-bit
+DU  *DS;                          ///< data stack pointer, Dr. Ting's stack
+DU  *RS;                          ///< return stack pointer, Dr. Ting's rack
 DU  top;                          ///< ALU (i.e. cached top of stack value)
 DU  rtop;                         ///< cached loop counter on return stack
 IU  IR;                           ///< interrupt service routine
 ///@}
-///
-///@name Storage
+///@name Memory Management Unit
 ///@{
 PGM_P _rom;                       ///< ROM, Forth word stored in Arduino Flash Memory
 U8    *_data;                     ///< RAM, memory block for user define dictionary
-DU    *DS;                        ///< data stack pointer, Dr. Ting's stack
-DU    *RS;                        ///< return stack pointer, Dr. Ting's rack
-///@}
-#define RAM_FLAG       0xe000     /**< RAM ranger      (0x2000~0x7fff) */
-#define OFF_MASK       0x07ff     /**< RAM offset mask (0x0000~0x07ff) */
-#define IRET_FLAG      0x8000     /**< interrupt return flag           */
-#define BOOL(f)        ((f) ? TRUE : FALSE)
-#define RAM(i)         &_data[(i) - FORTH_RAM_ADDR]
-///
-/// byte (8-bit) fetch from either RAM or ROM depends on filtered range
-///
-U8 BGET(U16 d) {
-    return (U8)((d&RAM_FLAG) ? _data[d&OFF_MASK] : pgm_read_byte(_rom+d));
-}
-///
-/// word (16-bit) fetch from either RAM or ROM depends on filtered range
-///
-U16 GET(U16 d) {
-    return (d&RAM_FLAG)
-        ? ((U16)_data[d&OFF_MASK]<<8) | _data[(d+1)&OFF_MASK]
-        : ((U16)pgm_read_byte(_rom+d)<<8) | pgm_read_byte(_rom+d+1);
-}
-#define BSET(d, c)     (_data[(d)&OFF_MASK]=(U8)(c))
-void SET(U16 d, U16 v) {
-	_data[(d)&OFF_MASK]   = v>>8;
-	_data[(d+1)&OFF_MASK] = v&0xff;
-}
-#define S2D(h, l)      (((S32)(h)<<16) | ((l)&0xffff))
-///
-/// push a value onto stack top
-///
-void PUSH(DU v)        { *++DS = top;  top  = v; }
-void RPUSH(DU v)       { *--RS = rtop; rtop = v; }
-#define POP()          (top  = *DS--)
-#define RPOP()         (rtop = *RS++)
-#define DTOP(d)        { *DS = (d) & 0xffff; top = (d)>>16; }
-///
-/// update program counter (ready to fetch), advance instruction pointer
-///
-void NEXT() { PC=GET(IP); IP+=sizeof(IU); }
-
-DU _depth() {
-    return (DU)((U8*)DS - RAM(FORTH_STACK_ADDR)) >> 1;
-}
-///
-///@name Tracing
-///@{
-#if EXE_TRACE
-int tCNT;           ///< tracing depth counters
-int tTAB;           ///< tracing indentation counter
-///@}
-///
-///@name Tracing Functions
-///@{
-#define opDOLIT 4
-#define opEXEC  6
-#define opENTER 7
-#define opEXIT  8
-
-#define DEBUG(s,v)  printf((s),(v))
-void TAB() {
-    LOG("\n");
-    for (int i=0; i<tTAB; i++) LOG("  ");
-}
-void TRACE(U8 op)
-{
-    if (!tCNT) return;                       /// * skip if not tracing or end of program
-    // indent call depth
-    if (op==opENTER) {
-    	TAB();
-    	tTAB++;
-    }
-    IU pc = (op==opENTER) ? (PC | BGET(IP)) : PC;
-    // display IP:PC[opcode]
-    LOG_H(" ", IP-1);                        /// * mem pointer
-    LOG_H(":", pc);                          /// * program counter, for indirect thread
-    LOG_H("[", op); LOG("]");                /// * opcode to be executed
-    // dump stack
-    DU s = _depth() - 1;                     ///< stack depth (minus top)
-    while (s-- > 0) {
-        DU *vp = ((DU*)DS - s);
-        DU v   = *vp;
-        LOG_H("_", v);
-    }
-    LOG_H("_", top);
-    LOG("_");
-    /// special opcode handlers for DOLIT, ENTER, EXIT
-    switch (op) {
-    case opDOLIT: LOG_H("$", GET(IP)); LOG(" "); break;
-    case opEXIT:  LOG(";");  --tTAB;             break;
-    case opEXEC: pc = top; /** no break */
-    case opENTER:                                 /// * display word name
-    	for (--pc; (BGET(pc) & 0x7f)>0x20; pc--); /// * retract pointer to word name (ASCII range: 0x21~0x7f)
-    	int len = BGET(pc++) & 0x1f;              /// Forth allows 31 char max
-    	for (int i=0; i<len; i++, pc++) {
-    		LOG_C((char)BGET(pc));
-    	}
-    	LOG(" :");
-        break;
-    }
-}
-#else
-#define opENTER 7
-#define DEBUG(s,v)
-#define TAB()         /* skip */
-#define TRACE(op)     /* skip */
-#endif // EXE_TRACE
 ///@}
 //
 // Forth Virtual Machine primitive functions
@@ -172,7 +48,7 @@ void _init() {
     ///   CP,  top of dictionary, same as HERE
     ///   CONTEXT name field of last word
     ///   LAST, same as CONTEXT
-    ///   'EVAL eval mode (interpreter or compiler)
+    ///   'MODE eval mode (interpreter or compiler)
     ///   'ABORT exception rescue handler (QUIT)
     ///   tmp storage (alternative to return stack)
     IU p = FORTH_UVAR_ADDR;           ///> setup Forth user variables
@@ -236,7 +112,7 @@ void _txsto()                ///> (c -- ) send a char to console
     POP();
 }
 ///@}
-void _ummod()               /// (udl udh u -- ur uq) unsigned divide of a double by single
+void _ummod()               /// (udl udh u -- ur uq) unsigned double divided by a single
 {
     U32 d = (U32)top;       ///> CC: auto variable uses C stack 
     U32 m = ((U32)*DS<<16) + (U16)*(DS-1);
@@ -278,7 +154,7 @@ void vm_init(PGM_P rom, U8 *data, void *io_stream) {
 ///   vm_outer - computed label jumps (25% faster than subroutine calls)
 ///   continue in _X() macro behaves as $NEXT
 ///
-#define OP(name)    &&L_##name /** redefined for label address */
+#define OP(name)    &&L_##name          /** redefined for label address     */
 #define _X(n, code) L_##n: { DEBUG("%s",#n); code; continue; }
 
 void vm_outer() {
@@ -418,7 +294,7 @@ void vm_outer() {
         _X(ONEP,  top++);
         _X(ONEM,  top--);
         _X(QDUP,  if (top) *++DS = top);
-        _X(DEPTH, DU d = _depth(); PUSH(d));
+        _X(DEPTH, DU d = DEPTH(); PUSH(d));
         _X(ULESS, top = BOOL((U16)*DS-- < (U16)top));
         _X(UMMOD, _ummod());              /// (udl udh u -- ur uq) unsigned divide of a double by single
         _X(UMSTAR,                        /// (u1 u2 -- ud) unsigned multiply return double product
---
+++ src/eforth_vm.h
@@ -0,0 +1,149 @@
+/**
+ * @file
+ * @brief eForth Virtual Machine module header
+ *
+ * ### eForth Memory map
+ *
+ * @code
+ *     0x0000-0x1fff ROM (8K Flash memory)
+ *     0x2000-0x24ff RAM (2K dynamic memory)
+ *         0x2000-0x201f User Variables
+ *         0x2020-0x23ff User Dictionary
+ *         0x2400-0x247f Data/Return Stacks
+ *         0x2480-0x24ff TIB (Terminal Input Buffer)
+ *         0x2500        heap
+ * @endcode
+ *
+ * ### Data and Return Stack
+ *
+ * @code
+ *            S                   R
+ *            |                   |
+ *    top -> [S0, S1, S2,..., R1, R0] <- rtop
+ * @endcode
+ * Note: Dr. Ting uses U8 (0~255) for wrap-around control
+ */
+#ifndef __EFORTH_VM_H
+#define __EFORTH_VM_H
+#include "eforth_core.h"
+
+namespace EfVM {
+///@name VM registers
+///@{
+extern IU  IP;                    ///< instruction pointer, IU is 16-bit, opcode is 8-bit
+extern IU  PC;                    ///< program counter, IU is 16-bit
+extern DU  *DS;                   ///< data stack pointer, Dr. Ting's stack
+extern DU  *RS;                   ///< return stack pointer, Dr. Ting's rack
+extern DU  top;                   ///< ALU (i.e. cached top of stack value)
+extern DU  rtop;                  ///< cached loop counter on return stack
+///@}
+///@name MMU
+///@{
+extern PGM_P _rom;                ///< ROM, Forth word stored in Arduino Flash Memory
+extern U8    *_data;              ///< RAM, memory block for user define dictionary
+///@}
+#define RAM_FLAG       0xe000     /**< RAM ranger      (0x2000~0x7fff) */
+#define OFF_MASK       0x07ff     /**< RAM offset mask (0x0000~0x07ff) */
+#define IRET_FLAG      0x8000     /**< interrupt return flag           */
+#define BOOL(f)        ((f) ? TRUE : FALSE)
+#define RAM(i)         &_data[(i) - FORTH_RAM_ADDR]
+///
+/// byte (8-bit) fetch from either RAM or ROM depends on filtered range
+///
+U8 BGET(U16 d) {
+    return (U8)((d&RAM_FLAG) ? _data[d&OFF_MASK] : pgm_read_byte(_rom+d));
+}
+///
+/// word (16-bit) fetch from either RAM or ROM depends on filtered range
+///
+U16 GET(U16 d) {
+    return (d&RAM_FLAG)
+        ? ((U16)_data[d&OFF_MASK]<<8) | _data[(d+1)&OFF_MASK]
+        : ((U16)pgm_read_byte(_rom+d)<<8) | pgm_read_byte(_rom+d+1);
+}
+#define BSET(d, c)     (_data[(d)&OFF_MASK]=(U8)(c))
+void SET(U16 d, U16 v) {
+	_data[(d)&OFF_MASK]   = v>>8;
+	_data[(d+1)&OFF_MASK] = v&0xff;
+}
+#define S2D(h, l) (((S32)(h)<<16) | ((l)&0xffff))
+///
+/// push a value onto stack top
+///
+DU   DEPTH()      { return (DU)((U8*)DS - RAM(FORTH_STACK_ADDR)) >> 1; }
+void PUSH(DU v)   { *++DS = top;  top  = v; }
+void RPUSH(DU v)  { *--RS = rtop; rtop = v; }
+#define POP()     (top  = *DS--)
+#define RPOP()    (rtop = *RS++)
+#define DTOP(d)   { *DS = (d)&0xffff; top = (d)>>16; }
+
+///
+///@name Tracing
+///@{
+#if EXE_TRACE
+int tCNT;           ///< tracing depth counters
+int tTAB;           ///< tracing indentation counter
+///@}
+///
+///@name Tracing Functions
+///@{
+#define opDOLIT 4
+#define opEXEC  6
+#define opENTER 7
+#define opEXIT  8
+
+#define DEBUG(s,v)  printf((s),(v))
+void TAB() {
+    LOG("\n");
+    for (int i=0; i<tTAB; i++) LOG("  ");
+}
+void TRACE(U8 op)
+{
+    if (!tCNT) return;                       /// * skip if not tracing or end of program
+    // indent call depth
+    if (op==opENTER) {
+    	TAB();
+    	tTAB++;
+    }
+    IU pc = (op==opENTER) ? (PC | BGET(IP)) : PC;
+    // display IP:PC[opcode]
+    LOG_H(" ", IP-1);                        /// * mem pointer
+    LOG_H(":", pc);                          /// * program counter, for indirect thread
+    LOG_H("[", op); LOG("]");                /// * opcode to be executed
+    // dump stack
+    DU s = DEPTH() - 1;                      ///< stack depth (minus top)
+    while (s-- > 0) {
+        DU *vp = ((DU*)DS - s);
+        DU v   = *vp;
+        LOG_H("_", v);
+    }
+    LOG_H("_", top);
+    LOG("_");
+    /// special opcode handlers for DOLIT, ENTER, EXIT
+    switch (op) {
+    case opDOLIT: LOG_H("$", GET(IP)); LOG(" "); break;
+    case opEXIT:  LOG(";");  --tTAB;             break;
+    case opEXEC: pc = top; /** no break */
+    case opENTER:                                 /// * display word name
+    	for (--pc; (BGET(pc) & 0x7f)>0x20; pc--); /// * retract pointer to word name (ASCII range: 0x21~0x7f)
+    	int len = BGET(pc++) & 0x1f;              /// Forth allows 31 char max
+    	for (int i=0; i<len; i++, pc++) {
+    		LOG_C((char)BGET(pc));
+    	}
+    	LOG(" :");
+        break;
+    }
+}
+#else
+    
+#define opENTER 7     /** for opcode reassignment in $NEXT */
+#define DEBUG(s,v)
+#define TAB()         /* skip */
+#define TRACE(op)     /* skip */
+    
+#endif // EXE_TRACE
+///@}
+    
+}; // namespace EfVM
+
+#endif // __EFORTH_VM_H

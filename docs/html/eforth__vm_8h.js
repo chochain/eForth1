6bcad71d9bf22e75241a4b57b213bf667e4e662f
gnii <chochain@yahoo.com>
2023-02-06 16:17:45 +0800

    ~/docs add eforth_vm

---
+++ docs/html/eforth__vm_8h.js
@@ -0,0 +1,23 @@
+var eforth__vm_8h =
+[
+    [ "RAM_FLAG", "eforth__vm_8h.html#ad572a768450b0c645d096d7e830c25cc", null ],
+    [ "IDX_MASK", "eforth__vm_8h.html#a6d325ff479ff96b8129435d7bbd85aa7", null ],
+    [ "IRET_FLAG", "eforth__vm_8h.html#a9d91438cfe35ddd3db827add653cf917", null ],
+    [ "BOOL", "eforth__vm_8h.html#a1f75956c7e91b6411417071ea93f7bfe", null ],
+    [ "RAM", "eforth__vm_8h.html#aaf6b91711b3d8b68c942eccd99b9d19e", null ],
+    [ "BSET", "eforth__vm_8h.html#a77270f01b515a878ec2853760cb20dfa", null ],
+    [ "S2D", "eforth__vm_8h.html#a35a26ccdccc5949a9553ce8547b3814c", null ],
+    [ "POP", "eforth__vm_8h.html#a2106695e949772f765546777ef34690e", null ],
+    [ "RPOP", "eforth__vm_8h.html#a55b5173e6face979deb7372dcbb643df", null ],
+    [ "DTOP", "eforth__vm_8h.html#a2a0ab46d7e9f1c39675adabb666df35d", null ],
+    [ "opENTER", "eforth__vm_8h.html#ae2e6932feda2f28548e9f5500ff8e898", null ],
+    [ "DEBUG", "eforth__vm_8h.html#a3254550e1d6d185f6b3e69b2d6468343", null ],
+    [ "TAB", "eforth__vm_8h.html#ad2e226689a02d2b5f1256e7e98f03cf5", null ],
+    [ "TRACE", "eforth__vm_8h.html#abf78c62d3da5e71ee2d75576561a8531", null ],
+    [ "BGET", "eforth__vm_8h.html#ab9cd2e7aef4c537d97137dafd02518ae", null ],
+    [ "GET", "eforth__vm_8h.html#a07e74ced8251d26cd20bce5a2c386d7a", null ],
+    [ "SET", "eforth__vm_8h.html#a6704f4eed222b21f8754d5c4711c1a15", null ],
+    [ "DEPTH", "eforth__vm_8h.html#a42d6bbbd96179a13b2fb9b0c9d58330a", null ],
+    [ "PUSH", "eforth__vm_8h.html#ac4f69cc49b68f10383886820029f6257", null ],
+    [ "RPUSH", "eforth__vm_8h.html#acfaf9ecd06a6d4b5dc4243201efaf38e", null ]
+];
---
+++ docs/html/group__Assembler.js
@@ -0,0 +1,11 @@
+var group__Assembler =
+[
+    [ "macros (to handle va_list)", "group__Pseudo.html", "group__Pseudo" ],
+    [ "opNOP", "group__Assembler.html#ga5c160dc5483bdf3d9ed57addbd21ca73ae2420e4b0c0ea017141da15d9d9c4ec6", null ],
+    [ "OPCODES", "group__Assembler.html#ga5c160dc5483bdf3d9ed57addbd21ca73a1a8ee31b1d4091a06d2c050dfc2fb541", null ],
+    [ "PC", "group__Assembler.html#ga77406fd9edef6925ff2bafc8746085eb", null ],
+    [ "R", "group__Assembler.html#ga85323f491b7b3ea3883bad0c91d9de17", null ],
+    [ "_link", "group__Assembler.html#gac44a2258aabda459216d48b8fa99e73a", null ],
+    [ "_byte", "group__Assembler.html#gad509ea2c54d134a1ef2f093eb460817f", null ],
+    [ "DOTQP", "group__Assembler.html#ga6b8f6e7e77a210d6bfaf73bb43897580", null ]
+];

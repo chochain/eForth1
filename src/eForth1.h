9a9dd1e43609456ec5f4de7c025ea237f02e0734
gnii <chochain@yahoo.com>
2023-02-18 13:31:22 +0800

    ~/src/eForth1.h add C interface

--- src/eForth1.h
+++ src/eForth1.h
@@ -1,6 +1,12 @@
 #ifndef __EFORTH1_H
 #define __EFORTH1_H
 
+typedef void (*CFP)();            ///> function pointer
+
+void vm_cfunc(int n, CFP fp);     ///< assign C interface (in slot n)
+void vm_push(int v);              ///< push value onto VM data stack
+int  vm_pop();                    ///< pop TOS off VM data stack
+
 #if ARDUINO
 #include <Arduino.h>
 #include <time.h>

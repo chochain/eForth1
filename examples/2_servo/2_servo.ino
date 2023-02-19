f39d11c382abeca0d39765cfaf55a78e101f8b96
gnii <chochain@yahoo.com>
2023-02-19 10:43:53 +0800

    ~/examples/2_servo update comments for forth

--- examples/2_servo/2_servo.ino
+++ examples/2_servo/2_servo.ino
@@ -1,7 +1,18 @@
+///
+/// @file
+/// @brief - eForth1 servo demo
+///
+/// from Wokwi: https://wokwi.com/projects/356866133593965569 , enter the following
+///   ok> : sync 7 for dup r@ 0 call next drop ;  \ sync 8 servos
+///   ok> : rnd clock drop abs 180 mod ;          \ randomize servo angle
+///   ok> : sweep rnd sync ;                      \ 8 servos in action
+///   ok> : ' sweep 50 tmisr                      \ make sweep as ISR at 500ms
+///   ok> : 1 timer                               \ timer on
+///
 #include <Servo.h>
 #include <eForth1.h>
 
-Servo sv[4];
+Servo sv[8];
 
 void servo() {
     int n = vm_pop();      ///> servo id
@@ -12,20 +23,20 @@ void servo() {
 void setup()
 {
     Serial.begin(115200);
-    while (!Serial);                   // wait for serial port to connect
+    while (!Serial);                   /// wait for serial port to connect
 
-    ef_setup();
+    ef_setup();                        /// * init eForth1
     
-    vm_cfunc(0, servo);
+    vm_cfunc(0, servo);                /// * register servo as cfunc[0]
 
-    for (int i=0; i<4; i++) { 
-        sv[i].attach(8+i);
+    for (int i=0; i<8; i++) { 
+        sv[i].attach(4+i);             /// * 4,5,6,...,11
     }
 }
 
 void loop()
 {
-    ef_run();
+    ef_run();                          /// * enter eForth1 outer interpreter
 }
 
 

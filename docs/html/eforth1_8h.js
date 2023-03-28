d7c19ee2b252ed5d5eca344eda34c2b40107c9c5
chochain <chochain@yahoo.com>
2023-03-27 19:28:25 -0700

    update from x400

--- docs/html/eforth1_8h.js
+++ docs/html/eforth1_8h.js
@@ -1,5 +1,9 @@
-var eforth1_8h =
+var eForth1_8h =
 [
-    [ "ef_setup", "eforth1_8h.html#a3be4743e748619321c1a4f95928e8de1", null ],
-    [ "ef_run", "eforth1_8h.html#a1fb0d84284dc4203d4a4e4dbe8e07b29", null ]
+    [ "CFP", "eForth1_8h.html#a2d422d478f77fc6c4aa3c9538683829d", null ],
+    [ "vm_cfunc", "eForth1_8h.html#ac274d9a96db09bfc7ec4635be50d3aa5", null ],
+    [ "vm_push", "eForth1_8h.html#a35fceadcc39370dc87d3be5608979925", null ],
+    [ "vm_pop", "eForth1_8h.html#a14b4544b998f4659f54969f571c58d8f", null ],
+    [ "ef_setup", "eForth1_8h.html#a3be4743e748619321c1a4f95928e8de1", null ],
+    [ "ef_run", "eForth1_8h.html#a1fb0d84284dc4203d4a4e4dbe8e07b29", null ]
 ];
